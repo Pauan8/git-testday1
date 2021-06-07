713d7b4bbd69fd2ecb01452be73bfd7753c557af
Pauan8 <pauline.j.andersson@gmail.com>
2021-06-07 13:20:11 +0200

    .

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(returnInt(6));
+    }
+
+    public static int returnInt(int x) {
+        return x + 1;
+    }
+}
