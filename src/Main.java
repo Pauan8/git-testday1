bc07eb119c7c6030e929267d4ac256a4a06a5cf2
Pauan8 <pauline.j.andersson@gmail.com>
2021-06-07 14:32:55 +0200

    message

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(returnInt(6));
+        System.out.println(increase(6));
+        System.out.println(multiply(6, 8));
     }
 
-    public static int returnInt(int x) {
+    public static int multiply(int x, int y){
+        return x*y;
+    }
+    public static int increase(int x) {
         return x + 1;
     }
+
+
 }
